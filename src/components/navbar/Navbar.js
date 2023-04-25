7907f1ee1748e47e3ddd996c806a5075adc3c691
Anon7438 <thedashinghacker@gmail.com>
2023-04-25 20:20:32 +0530

    toggle button

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,39 +1,29 @@
+import React from "react";
 
-import React  from "react";
-
-import "./navbar.css"
-
-
-
+import "./navbar.css";
 
 const Navbar = () => {
-
+  const  myFunction =()=> {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  };
   return (
-    <React.Fragment>
-     
-      <div className="navbar">
-        <div className="logo">
-        <img src="https://img.icons8.com/clouds/50/null/anonymous-mask.png" alt="logo"/>
-        </div>
-        <div className="nav-items">
-              <label htmlFor="check" className="check_btn">                
-                  <input type="checkbox" className="checkbox" id="openMenuBar" />
-                  
-               
-               
-          <ul>
-            <li>  <a href="/home">Home</a> </li>
-            <li>  <a href="www.google.com">About</a>        </li>
-            <li>  <a href="www.google.com">Blogs</a>        </li>
-            <li>  <a href="www.google.com">Contact</a>      </li>
-            <li>  <a href="www.google.com">Feedback</a>     </li>
-          </ul>
-          </label>
-        </div>        
+    <>
+      <div className="topnav" id="myTopnav">
+        <a href="/home" className="active">  Home </a>
+        <a >News</a>
+        <a >Contact</a>
+        <a >About</a>
+        <a >Feedback</a>
+        <a className="icon" onClick={myFunction}>
+          <i className="fa fa-bars" />
+        </a>
       </div>
-
- 
-    </React.Fragment>
+    </>
   );
 };
 
